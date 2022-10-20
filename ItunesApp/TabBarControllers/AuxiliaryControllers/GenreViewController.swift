8d82c2005cc76878328f370648ec45b916701826
Nikolai Maximov <kolkamaximov@gmail.com>
2022-10-20 21:12:44 +0300

    create Genre View Controller

---
+++ ItunesApp/TabBarControllers/AuxiliaryControllers/GenreViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GenreViewController.swift
+//  ItunesApp
+//
+//  Created by Nikolai Maksimov on 20.10.2022.
+//
+
+import UIKit
+
+class GenreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemMint
+        
+        title = "Жанры"
+    }
+    
+
+   
+
+}
