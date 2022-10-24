77bf1d87c26662a817d83ae3b007c51f61bd391c
Nikolai Maximov <kolkamaximov@gmail.com>
2022-10-24 22:41:44 +0300

    add showAllAlbumsButton in cell

--- ItunesApp/TabBarControllers/Cells/CollectionViewTableViewCell.swift
+++ ItunesApp/TabBarControllers/Cells/CollectionViewTableViewCell.swift
@@ -18,6 +18,17 @@ class CollectionViewTableViewCell: UITableViewCell {
         return label
     }()
     
+    private lazy var showAllAlbumsButton: UIButton = {
+       var attributes = AttributeContainer()
+        attributes.font = UIFont.systemFont(ofSize: 12)
+        
+        var buttonConfiguration = UIButton.Configuration.borderless()
+        buttonConfiguration.attributedTitle = AttributedString("См.все>", attributes: attributes)
+        return UIButton(configuration: buttonConfiguration, primaryAction: UIAction {_ in
+            print("Show all albums")
+        })
+    }()
+    
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -32,11 +43,10 @@ class CollectionViewTableViewCell: UITableViewCell {
         
         contentView.addSubview(collectionView)
         contentView.addSubview(titleLabel)
+        contentView.addSubview(showAllAlbumsButton)
         
         collectionView.delegate = self
         collectionView.dataSource = self
-        
-        
         
         collectionView.showsHorizontalScrollIndicator = false
         
@@ -46,12 +56,6 @@ class CollectionViewTableViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-       
-    }
-    
 }
 
 extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -65,7 +69,6 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
         
         return cell
     }
-    
 }
 
 extension CollectionViewTableViewCell {
@@ -82,6 +85,13 @@ extension CollectionViewTableViewCell {
             collectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             collectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             collectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+        ])
+        
+        showAllAlbumsButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            showAllAlbumsButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            showAllAlbumsButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
         ])
     }
 }
