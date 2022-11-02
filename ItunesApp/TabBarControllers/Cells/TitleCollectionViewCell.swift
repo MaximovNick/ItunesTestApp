6a84157582221c6965bb2dfc4bd740a68ff2e487
Nikolai Maximov <kolkamaximov@gmail.com>
2022-11-02 23:14:11 +0300

    add 2 labels in collection cell

--- ItunesApp/TabBarControllers/Cells/TitleCollectionViewCell.swift
+++ ItunesApp/TabBarControllers/Cells/TitleCollectionViewCell.swift
@@ -10,16 +10,36 @@ import UIKit
 class TitleCollectionViewCell: UICollectionViewCell {
     static let identifier = "TitleViewCell"
     
-    
     private let posterImageView: UIImageView = {
        let imageView = UIImageView()
         imageView.backgroundColor = .red
         return imageView
     }()
     
+    private let trackTitleLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Название трека"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    private let artistTitleLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Имя артиста"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.addSubview(posterImageView)
+        addSubview(trackTitleLabel)
+        addSubview(artistTitleLabel)
+        
+        
+        
     }
     
     required init?(coder: NSCoder) {
@@ -29,5 +49,13 @@ class TitleCollectionViewCell: UICollectionViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         posterImageView.frame = contentView.bounds
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            artistTitleLabel.topAnchor.constraint(equalTo: posterImageView.bottomAnchor, constant: 0),
+            artistTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            artistTitleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 16)
+        ])
     }
 }
